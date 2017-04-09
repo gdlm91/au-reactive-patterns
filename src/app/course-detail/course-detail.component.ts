6275185f92b0ca9e0e444908c9edf498fdf6e801
Gerardo Leal <gerard.leal91@gmail.com>
2017-04-09 16:32:51 -0300

    Observable Data Service Done

--- src/app/course-detail/course-detail.component.ts
+++ src/app/course-detail/course-detail.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
-import {Course} from "../shared/model/course";
-import {Lesson} from "../shared/model/lesson";
+import { ActivatedRoute } from '@angular/router';
+import { Course } from "../shared/model/course";
+import { Lesson } from "../shared/model/lesson";
 import * as _ from 'lodash';
-import {CoursesService} from "../services/courses.service";
-import {NewsletterService} from "../services/newsletter.service";
+import { CoursesService } from "../services/courses.service";
+import { NewsletterService } from "../services/newsletter.service";
+import { Observable } from 'rxjs';
+import { User } from '../shared/model/user';
+import { UserService, UNKNOWN_USER } from '../services/user.service';
 
 
 @Component({
@@ -16,40 +19,42 @@ export class CourseDetailComponent implements OnInit {
 
   course: Course;
   lessons: Lesson[];
+  user$: Observable<User>
 
   constructor(private route: ActivatedRoute,
-              private coursesService: CoursesService,
-              private newsletterService: NewsletterService) {
+    private coursesService: CoursesService,
+    private newsletterService: NewsletterService,
+    private userService: UserService
+  ) { }
 
-      route.params
-          .subscribe( params => {
-
-              const courseUrl = params['id'];
-
-              this.coursesService.findCourseByUrl(courseUrl)
-              .subscribe(data => {
-                  this.course = data;
-
-                  this.coursesService.findLessonsForCourse(this.course.id)
-                    .subscribe(lessons => this.lessons = lessons);
-              });
-
-          });
-
-  }
-
-  onSubscribe(email:string) {
-      this.newsletterService.subscribeToNewsletter(email)
-          .subscribe(
-              () => {
-                  alert('Subscription successful ...');
-              },
-              console.error
-          );
+  onSubscribe(email: string) {
+    this.newsletterService.subscribeToNewsletter(email)
+      .subscribe(
+      () => {
+        alert('Subscription successful ...');
+      },
+      console.error
+      );
   }
 
   ngOnInit() {
 
+    this.user$ = this.userService.user$;
+
+    this.route.params
+      .subscribe(params => {
+
+        const courseUrl = params['id'];
+
+        this.coursesService.findCourseByUrl(courseUrl)
+          .subscribe(data => {
+            this.course = data;
+
+            this.coursesService.findLessonsForCourse(this.course.id)
+              .subscribe(lessons => this.lessons = lessons);
+          });
+
+      });
   }
 
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,21 +1,30 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from '../services/user.service';
 
 @Component({
-    selector: 'login',
-    templateUrl: './login.component.html',
-    styleUrls: ['./login.component.css']
+  selector: 'login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
-    constructor() {
-    }
+  constructor(
+    private userService: UserService,
+    private router: Router
+  ) { }
 
-    ngOnInit() {
+  ngOnInit() { }
 
-    }
-
-    login() {
-
-    }
+  login(email: string, password: string) {
+    this.userService.login(email, password)
+      .subscribe(
+        () => {
+          alert('Login Successful');
+          this.router.navigateByUrl('/home');
+        },
+        console.error
+      );
+  }
 
 }
