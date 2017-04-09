b4e456a6c6d6d52312ef20e1ab8477854f5e3952
Gerardo Leal <gerard.leal91@gmail.com>
2017-04-09 12:28:54 -0300

    Delete and Toggle Method on DataStore

--- src/app/event-bus-experiments/app-data.ts
+++ src/app/event-bus-experiments/app-data.ts
@@ -46,12 +46,28 @@ class DataStore {
 
   initializeLessonsList(newList: Lesson[]) {
     this.lessons = _.cloneDeep(newList);
-    this.lessonsListSubject.next(this.lessons);
+    this.broadcast();
   }
 
   addLesson(newLesson: Lesson) {
     this.lessons.push(_.cloneDeep(newLesson));
-    this.lessonsListSubject.next(this.lessons);
+    this.broadcast();
+  }
+
+  deleteLesson(deleted: Lesson) {
+    _.remove(this.lessons, lesson => lesson.id === deleted.id);
+    this.broadcast();
+  }
+
+  toggleLessonViewed(toggled: Lesson) {
+    const lesson = _.find(this.lessons, lesson => lesson.id === toggled.id);
+
+    lesson.completed = !lesson.completed;
+    this.broadcast();
+  }
+
+  broadcast() {
+    this.lessonsListSubject.next(_.cloneDeep(this.lessons));
   }
 
 }
