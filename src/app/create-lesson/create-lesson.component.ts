c7d32239d106dbd466803ce167d14603c1020fea
Gerardo Leal <gerardo.leal@synapse.com>
2017-05-11 16:58:29 -0300

    Partial Solution

--- src/app/create-lesson/create-lesson.component.ts
+++ src/app/create-lesson/create-lesson.component.ts
@@ -1,5 +1,5 @@
-import {Component, OnInit} from '@angular/core';
-import {FormGroup, FormBuilder, Validators} from "@angular/forms";
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
     selector: 'create-lesson',
@@ -8,20 +8,31 @@ import {FormGroup, FormBuilder, Validators} from "@angular/forms";
 })
 export class CreateLessonComponent implements OnInit {
 
+    private static readonly DRAFT_STORAGE = 'create-lesson-draft';
+
     form: FormGroup;
 
     constructor(private fb: FormBuilder) {
 
         this.form = this.fb.group({
-            description: ['',Validators.required],
-            url: ['',Validators.required],
+            description: ['', Validators.required],
+            url: ['', Validators.required],
             longDescription: ['']
         });
 
     }
 
     ngOnInit() {
+        const draft = sessionStorage.getItem(CreateLessonComponent.DRAFT_STORAGE);
 
+        if(draft) {
+            this.form.setValue(JSON.parse(draft));
+        }
+
+        this.form.valueChanges
+        .filter(() => this.form.valid)
+        .do(validValue => sessionStorage.setItem(CreateLessonComponent.DRAFT_STORAGE, JSON.stringify(validValue)))
+        .subscribe();
     }
 
 }
